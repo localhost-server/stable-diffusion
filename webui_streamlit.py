2a9e21d54ed77ad1c345ab1ec925825f28293441
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-12 14:39:32 +0530

    Update webui_streamlit.py

--- webui_streamlit.py
+++ webui_streamlit.py
@@ -17,7 +17,7 @@
 # base webui import and utils.
 #import streamlit as st
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 device = "cuda"
 # We import hydralit like this to replace the previous stuff
 # we had with native streamlit as it lets ur replace things 1:1
