f53f2837ea6116391d32a95a15a4e6795376b53c
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-15 14:22:40 +0530

    Update ModelManager.py

--- scripts/ModelManager.py
+++ scripts/ModelManager.py
@@ -96,8 +96,8 @@ def layout():
     if st.button("reset"):
         if os.path.exists('configs/webui/userconfig_streamlit.yaml'):
             os.remove('configs/webui/userconfig_streamlit.yaml')
-        if os.path.exists('models/custom/waifu-diffusion.ckpt'):
-            os.remove('models/custom/waifu-diffusion.ckpt')
+        if os.path.exists('models/custom/WaifuDiffusion-V1.3.ckpt'):
+            os.remove('models/custom/WaifuDiffusion-V1.3.ckpt')
         
     else:
         pass
