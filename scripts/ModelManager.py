5459092ccb5e2cb7f912611a5042313e4eda43a3
localhost-server <99524394+localhost-server@users.noreply.github.com>
2022-10-16 14:44:20 +0530

    Update ModelManager.py

--- scripts/ModelManager.py
+++ scripts/ModelManager.py
@@ -100,6 +100,7 @@ def layout():
             shutil.rmtree('outputs/')
             if os.path.exists('models/clip'):
                 shutil.rmtree('models/clip/')
+                
             if os.path.exists('outputs.7z'):
                 os.remove("outputs.7z")
         os.system("pip3 cache purge")
